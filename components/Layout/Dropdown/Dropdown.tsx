85f71ad084233fe7778e5be853c534dbde587ad8
MrFarhan <muhammadfarhan012345@gmail.com>
2021-09-09 21:13:18 +0500

    Improved dropdown

--- components/Layout/Dropdown/Dropdown.tsx
+++ components/Layout/Dropdown/Dropdown.tsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import styles from "./Dropdown.module.scss"
+import React from "react";
+import styles from "./Dropdown.module.scss";
 import { BiChevronDown } from "react-icons/bi";
 
-
-
 const Dropdown = () => {
-    return (
-        <div className={styles.dropdownMain}>Dropdown here <BiChevronDown/>
-            {/* <div>Item 1</div>    
-            <div>Item 1</div>    
-            <div>Item 1</div>    
-            <div>Item 1</div>    
-            <div>Item 1</div>     */}
-        </div>
-    )
-}
+  const [showMenu, setShowMenu] = React.useState(false);
+  console.log("show menu", showMenu);
+  return (
+    <div className={styles.dropdownMain} onClick={() => setShowMenu(!showMenu)}>
+      <nav>
+        <span
+          className={showMenu ? styles.categoryNavHovered : styles.categoryNav}
+        >
+          All Categories
+          <BiChevronDown />
+          <ul>
+            <li>All Categories</li>
+            <li>Car</li>
+            <li>Electronics</li>
+            <li>Laptop</li>
+            <li>Desktop</li>
+            <li>Camera</li>
+            <li>Toys</li>
+          </ul>
+        </span>
+      </nav>
+    </div>
+  );
+};
 
-export default Dropdown
+export default Dropdown;
