caf9e082dd01405dede40ec92b5c59c6dc722b0e
MrFarhan <muhammadfarhan012345@gmail.com>
2021-10-10 00:45:07 +0500

    Improved heart icon

--- components/EyeHeartGroup/EyeHeartGroup.tsx
+++ components/EyeHeartGroup/EyeHeartGroup.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import {IoMdEye} from "react-icons/io"
-import {AiOutlineHeart} from "react-icons/ai"
-import styles from "./EyeHeartGroup.module.scss"
+import React, { useState } from "react";
+import { IoMdEye } from "react-icons/io";
+import { AiFillHeart } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
+import styles from "./EyeHeartGroup.module.scss";
 
 const EyeHeartGroup = () => {
+  const [heartFill, setHeartFill] = useState(false);
   return (
     <div className={styles.eyeHeartGroupMain}>
-      <IoMdEye/> &nbsp; <AiOutlineHeart/>
+      <span className={styles.eye}>
+        <span className={styles.icon}>
+          <IoMdEye size="21px" />{" "}
+        </span>
+      </span>
+      <span
+        className={styles.heart}
+        onClick={() => setHeartFill((prev) => !prev)}
+      >
+        {heartFill ? (
+          <span className={styles.icon}>
+            <AiFillHeart color={"red"} size="21px" />
+          </span>
+        ) : (
+          <span className={styles.icon}>
+            <AiOutlineHeart size="21px" />
+          </span>
+        )}
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default EyeHeartGroup
+export default EyeHeartGroup;
